fda1eacd3a54b66d53480963c9dc09b046c91a79
Daniel <cesardsaavedra@gmail.com>
2025-10-11 21:05:03 -0300

    se agrega Headr base vacio para comenzar a construirlo

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+
+export default function Header() {
+  return (
+    <div>
+      {/* Header en construcción */}
+    </div>
+  );
+}
