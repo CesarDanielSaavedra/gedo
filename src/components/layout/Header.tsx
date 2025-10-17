6959ad4edac21a75b1f55b4d8b1a0fcf9236ff57
Daniel <cesardsaavedra@gmail.com>
2025-10-16 21:27:51 -0300

    se implementa el Header con menú, notificaciones y avatar de usuario

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,41 @@
 "use client";
 
-import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Box, Badge, Avatar } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 export default function Header() {
   return (
-    <div>
-      {/* Header en construcción */}
-    </div>
+    <AppBar>
+        <Toolbar>
+            {/* Menú hamburguesa */}
+            <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                <MenuIcon />
+            </IconButton>
+
+            {/* Logo GEDO */}
+            <Typography variant="h6" component="div">
+                GEDO
+            </Typography>
+
+            {/* Espaciador */}
+            <Box sx={{ flexGrow: 1 }} />
+
+            {/* Icono de notificaciones */}
+            <IconButton size="large" color="inherit">
+                <Badge color="warning" variant="dot">
+                    <NotificationsIcon />
+                </Badge>
+            </IconButton>
+
+            {/* Avatar de usuario */}
+            <IconButton size="large" color="inherit">
+                <Avatar alt="Usuario" src="">
+                    {/* DG */}
+                </Avatar>
+            </IconButton>
+        </Toolbar>
+    </AppBar>
+    
   );
 }
